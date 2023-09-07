0617aec57365d5d13f5e26736bfe81a06f2ffc8d
nikitakley <nikitakley@gmail.com>
2023-09-07 19:34:29 +0300

    lab0

---
+++ Kleymenov_lab0/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	string n;
+	cout << "Hello, world!\n";
+	cout << "Write your text:";
+	cin >> n;
+	cout << "Your text: " << n << "\n";
+	return 0;
+}
